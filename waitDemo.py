49b7e013071af740a36b039a9b4c60b28baa9042
cordovabarbara <aliendocordova@gmail.com>
2025-09-01 18:25:41 -0500

    Añadir WebDriverWait explícito para elemento .promoInfo

--- waitDemo.py
+++ waitDemo.py
@@ -1,9 +1,11 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 
 driver = webdriver.Chrome()
-driver.implicitly_wait(5)
+driver.implicitly_wait(2)
 
 driver.get("https://rahulshettyacademy.com/seleniumPractise/#/")
 driver.maximize_window()
@@ -25,6 +27,8 @@ driver.find_element(By.XPATH, "//button[text()='PROCEED TO CHECKOUT']").click()
 # Aplicar cupón
 driver.find_element(By.CSS_SELECTOR, ".promoCode").send_keys("rahulshettyacademy")
 driver.find_element(By.CSS_SELECTOR, ".promoBtn").click()
+wait = WebDriverWait(driver, 10)
+wait.until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR,".promoInfo")))
 
 print(driver.find_element(By.CLASS_NAME, "promoInfo").text)
 
