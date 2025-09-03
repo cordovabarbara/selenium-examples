323228125c913252fc2478c34c396e359f7b542d
cordovabarbara <aliendocordova@gmail.com>
2025-09-03 09:15:53 -0500

    Añadir verificación de cálculo de total del carrito

--- waitDemo.py
+++ waitDemo.py
@@ -24,7 +24,16 @@ for result in results:
 driver.find_element(By.CSS_SELECTOR, "img[alt='Cart']").click()
 driver.find_element(By.XPATH, "//button[text()='PROCEED TO CHECKOUT']").click()
 
-# Aplicar cupón
+#Sum validation
+prices = driver.find_elements(By.CSS_SELECTOR, "tr td:nth-child(5) p")
+sum = 0
+for price in prices:
+    sum = sum +int(price.text)
+
+print(sum)
+totalAmount = int(driver.find_element(By.CSS_SELECTOR, ".totAmt").text)
+assert sum == totalAmount
+
 driver.find_element(By.CSS_SELECTOR, ".promoCode").send_keys("rahulshettyacademy")
 driver.find_element(By.CSS_SELECTOR, ".promoBtn").click()
 wait = WebDriverWait(driver, 10)
