f068abb453b4b3631415a7d7ea74e49debe9e64c
cordovabarbara <aliendocordova@gmail.com>
2025-09-03 11:49:34 -0500

    Agregar manejo de múltiples ventanas con Selenium

---
+++ childWindow.py
@@ -0,0 +1,22 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.wait import WebDriverWait
+
+driver = webdriver.Chrome()
+driver.get("https://the-internet.herokuapp.com/windows")
+driver.maximize_window()
+driver.implicitly_wait(3)
+
+driver.find_element(By.LINK_TEXT, "Click Here").click()
+windowsOpened = driver.window_handles
+
+driver.switch_to.window(windowsOpened[1])
+print(driver.find_element(By.TAG_NAME, "h3").text)
+
+
+driver.quit()
+
+
