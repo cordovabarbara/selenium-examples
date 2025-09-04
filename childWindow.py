9dca3a09469ecd99a6eb5e1d2b31d8dd9f3699ab
cordovabarbara <aliendocordova@gmail.com>
2025-09-04 11:48:02 -0500

    Agregar assert en ventana principal y cerrar navegador con driver.quit()

--- childWindow.py
+++ childWindow.py
@@ -1,9 +1,6 @@
-import time
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.select import Select
-from selenium.webdriver.support.wait import WebDriverWait
+
 
 driver = webdriver.Chrome()
 driver.get("https://the-internet.herokuapp.com/windows")
@@ -15,7 +12,10 @@ windowsOpened = driver.window_handles
 
 driver.switch_to.window(windowsOpened[1])
 print(driver.find_element(By.TAG_NAME, "h3").text)
+driver.close()
 
+driver.switch_to.window(windowsOpened[0])
+assert "Opening a new window" == driver.find_element(By.TAG_NAME, "h3").text
 
 driver.quit()
 
