7839f4230946dd45f932ec004030ee7af96e5ef7
cordovabarbara <aliendocordova@gmail.com>
2025-09-05 15:35:31 -0500

    agregar ejemplo de manejo de scroll con Selenium

---
+++ scroll_handling.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+
+
+chrome_options = webdriver.ChromeOptions()
+#chrome_options.add_argument("headless")
+driver = webdriver.Chrome()
+
+driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+
+driver.execute_script("window.scrollBy(0,document.body.scrollHeight);")
+driver.get_screenshot_as_file("screen.png")
