7ff2c383bfac9484f0ddc383812241428dc0ce86
cordovabarbara <aliendocordova@gmail.com>
2025-09-08 21:22:18 -0500

    agregar extracción dinámica de precios con XPath avanzado

--- excelDemo.py
+++ excelDemo.py
@@ -3,15 +3,17 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 
+
 file_path = r"C:\Users\Anais\Downloads\download.xlsx"
+fruit_name = 'Apple'
 
 driver = webdriver.Chrome()
-driver.implicitly_wait(6)
+driver.implicitly_wait(5)
 driver.get ("https://rahulshettyacademy.com/upload-download-test/index.html")
 
 driver.find_element(By.ID, "downloadButton").click()
 
-#edit excel
+#edicion de excel con value
 file_input = driver.find_element(By.CSS_SELECTOR,"input[type='file']")
 file_input.send_keys(file_path)
 
@@ -19,8 +21,9 @@ wait = WebDriverWait(driver, 6)
 toast_locator = (By.CSS_SELECTOR,".Toastify__toast-body div:nth-child(2)")
 wait.until(EC.visibility_of_element_located(toast_locator))
 print(driver.find_element(*toast_locator).text)
-
-
+price_colum = driver.find_element(By.XPATH,"//div[text()='Price']").get_attribute("data-column-id")
+actual_price = driver.find_element(By.XPATH,"//div[text()='"+fruit_name+"']/ancestor::div[@role='row']//div[@data-column-id='"+price_colum+"']").text
+print(actual_price)
 
 
 
--- exercise2.py
+++ exercise2.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
@@ -9,7 +11,7 @@ driver = webdriver.Chrome()
 
 driver.get("https://rahulshettyacademy.com/loginpagePractise/")
 driver.maximize_window()
-driver.implicitly_wait(4)
+driver.implicitly_wait(7)
 
 #Click en el link
 driver.find_element(By.PARTIAL_LINK_TEXT, "Free Access").click()
@@ -19,6 +21,7 @@ windowsOpened = driver.window_handles
 
 #Cambiar a la nueva ventana
 driver.switch_to.window(windowsOpened[1])
+time.sleep(6)
 
 #Obtener el correo en la nueva ventana
 email = driver.find_element(By.CSS_SELECTOR,"p.im-para.red a").text
@@ -27,6 +30,7 @@ print("Correo encontrado:", email)
 #Cerrar Ventana
 driver.close()
 driver.switch_to.window(windowsOpened[0])
+
 
 #pegar texto en el input
 driver.find_element(By.ID, "username").send_keys(email)
